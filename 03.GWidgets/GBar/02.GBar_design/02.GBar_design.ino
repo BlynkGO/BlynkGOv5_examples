5bc375a8017579afca460155d3b69b108541d18d
BlynkGO <blynkgo@gmail.com>
2025-04-18 23:58:50 +0700

    update GBar

--- 03.GWidgets/GBar/02.GBar_design/02.GBar_design.ino
+++ 03.GWidgets/GBar/02.GBar_design/02.GBar_design.ino
@@ -4,23 +4,18 @@ GBar bar;
 
 void setup()
 {
-  Serial.begin(9600);
-  Serial.println();
+  Serial.begin(9600); Serial.println();
   BlynkGO.begin();
   BlynkGO.fillScreen(TFT_WHITE);
 
-  bar.removeStyleAll();
+  bar.size(200, 20);
   bar.border(2, TFT_PALETTE(TFT_PALETTE_BLUE), GPART_MAIN);
   bar.padding(6,  GPART_MAIN);
   bar.radius(6, GPART_MAIN);
   bar.anim_duration(1000,  GPART_MAIN);
-
-  bar.opa(OPA_COVER, GPART_INDICATOR);
   bar.color(TFT_PALETTE(TFT_PALETTE_BLUE), GPART_INDICATOR);
   bar.radius(6, GPART_INDICATOR);
-
-  bar.size(200, 20);
-  bar.align_center(0, 30);
+  bar.range(0,100);
   bar.value(100, true);
 }
 
@@ -28,5 +23,4 @@ void loop()
 {
   BlynkGO.update();
 }
-
 
