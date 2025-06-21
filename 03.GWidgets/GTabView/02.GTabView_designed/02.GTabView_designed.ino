abb6d4c5c395822dbae09cbda17df897d81c969b
BlynkGO <blynkgo@gmail.com>
2025-06-21 07:58:16 +0700

    Update 02.GTabView_designed.ino

--- 03.GWidgets/GTabView/02.GTabView_designed/02.GTabView_designed.ino
+++ 03.GWidgets/GTabView/02.GTabView_designed/02.GTabView_designed.ino
@@ -38,21 +38,25 @@ void setup(){
   BlynkGO.begin();
 
   tabview.size(PCT(100),PCT(100));
+  // tabview.tab_bar_position(DIR_BOTTOM);  // วาง tab_bar ไว้ด้านล่าง
+  // tabview.tab_bar_position(DIR_LEFT);    // วาง tab_bar ไว้ด้านซ้าย
+  // tabview.tab_bar_position(DIR_RIGHT);   // วาง tab_bar ไว้ด้านขวา
+  tabview.tab_bar_size(40);                 // ความสูงของ tab_bar หากอยู่ด้านบนหรือล่าง , ความกว้างของ tab_bar หากอยู่ซ้ายหรือขวา
     tab_bar = tabview.tab_bar();
     tab_bar.font(google_icon_basic_20);
-    tab_bar.height(40);
 
     // สร้าง tab
     tab_home  = tabview.add_tab(SYMBOL_HOME);                  // เพิ่ม tab ใหม่ ใช้ icon จาก font แทน ข้อความ 
     tab_admin = tabview.add_tab(SYMBOL_ADMIN_PANEL_SETTINGS);
     tab_wifi  = tabview.add_tab(SYMBOL_WIFI);
 
-    // ปรับแต่งดีไซน์ ให้ปุ่มบน tab
+    // ปรับแต่งดีไซน์ ให้ปุ่มบน tab ให้ปรับแต่งดีไซน์ที่ GSTATE_ON ของปุ่ม
     for(int i=0; i< tab_bar.child_count(); i++){
       tab_btn = tab_bar.get_child_by_type(i, BUTTON_CLASS);
       tab_btn.bg_opa(0, GSTATE_ON);                                           // ไม่ต้องมีสีพื้นหลัง
       tab_btn.font_color(TFT_PALETTE(TFT_PALETTE_INDIGO), GSTATE_ON);         // สีของ font บนปุ่ม ที่ tab bar
       tab_btn.border_color(TFT_PALETTE(TFT_PALETTE_INDIGO), GSTATE_ON);       // สีของ border บนปุ่มที่ tab bar
+      // tab_btn.border_side(BORDER_SIDE_RIGHT, GSTATE_ON);                   // ให้ปุ่มบน tab bar มีขอบเฉพาะด้านขวา
     }
 
     tab_home.layout(LAYOUT_COL_L,10,10,10,10,0);
